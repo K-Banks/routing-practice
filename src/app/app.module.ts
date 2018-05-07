afb6f2cc83ff0116a7ca95247f15c0d741164fa8
Cam Button and Kayl Eubanks <cam@>
2018-05-07 10:29:59 -0700

    add new components/routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,20 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { routing } from './app.routing';
+import { AboutComponent } from './about/about.component';
+import { MarketplaceComponent } from './marketplace/marketplace.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    AboutComponent,
+    MarketplaceComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
