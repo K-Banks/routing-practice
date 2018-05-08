2b23920340e4eba5249eb961c3a59637d8d498fd
Cam Button and Kayl Eubanks <cam@>
2018-05-07 17:45:48 -0700

    Attempting to link FireBase

---
+++ src/app/api-keys.ts
@@ -0,0 +1,7 @@
+export var masterFirebaseConfig = {
+    apiKey: "AIzaSyB9TfDZMYoXtUNR4X9l7qa2j1rMPDmymEI",
+    authDomain: "my-example-8668b.firebaseapp.com",
+    databaseURL: "https://my-example-8668b.firebaseio.com",
+    storageBucket: "my-example-8668b.appspot.com",
+    messagingSenderId: "934542429209"
+  };
